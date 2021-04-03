f57fdc1878e31ad29e59ee6410f6e2b128d7ffa3
Hemang Dave <hemangjdave@gmail.com>
2021-04-03 17:56:47 +0530

    Creating initial project with spring web and lombok

---
+++ src/test/java/com/techrevolution/nqueens/NQueensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techrevolution.nqueens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NQueensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
